84b7661337c745d38819d1aae6df8ba24c23fcdf
Lee <whdtnxd@gmail.com>
2024-04-22 00:09:52 +0900

    24 04 22 / 12:09AM

--- JS/app.mjs
+++ JS/app.mjs
@@ -1,17 +1,23 @@
 import * as mkUserInfo from "./modules/mkUserInfo.js";
 
+// * 폼 내 element들 객체화
 export const aboutInputForm = {
   inputForm: document.querySelector(`.input_form`),
   userName: document.querySelector(`.input_form > input:first-child`),
   userCode: document.querySelector(`.input_form > input:nth-child(2)`),
-  submitBtn: document.querySelector(`.input_form > input:nth-child(3)`),
+  submitBtn: document.querySelector(`.input_form > input[type=submit]`),
 };
 
+// * 폼 서브밋 이벤트 감지
 aboutInputForm.inputForm.onsubmit = (e) => {
   e.preventDefault();
   const inputFormValues = {
     userNameValue: aboutInputForm.userName.value,
     userCodeValue: aboutInputForm.userCode.value,
   };
+  // * 모듈에서 임포트된 msUserInfo.js내 함수에, 인자로 인풋 벨류들 전달
   mkUserInfo.userInfoFunc(inputFormValues);
 };
+
+// * 모듈에서 처리된 후 마지막 결과 띄우기
+export const resultTest = (result) => console.log(result);
--- JS/modules/mkUserInfo.js
+++ JS/modules/mkUserInfo.js
@@ -1,5 +1,4 @@
-let userName;
-let userCode;
+import * as app from "../app.mjs";
 
 class User {
   constructor(userInfo) {
@@ -7,20 +6,40 @@ class User {
     this.userCode = userInfo.userCodeValue;
   }
 
-  saveUserInfo(newUserArr) {
-    console.log(newUserArr, 1);
-    let test = newUserArr.map((user) => console.log(user[0]));
-  }
+  // saveUserInfo(newUserArr) {
+  //   let userName = newUserArr[0];
+  //   let userCode = newUserArr[1];
+  //   // console.log(this);
+  //   // console.log(newUserArr);
+  //   // // console.log(newUserArr, 1);
+  //   // newUserArr.forEach((user) => {
+  //   //   // userName = user;
+  //   //   // userCode = user;
+  //   //   if (userName !== null) {
+  //   //     userName = user;
+  //   //   } else if (userCode !== null) {
+  //   //     userCode = user;
+  //   //   } else {
+  //   //   }
+  //   // });
+  //   // console.log(userName, userCode);
+  // }
 }
 
+// * app.mjs에서 처음으로 실행되는 함수
 export const userInfoFunc = (inputFormValues) => {
   const newUserArr = new Array();
   const newUserInfo = new User(inputFormValues);
+  // * 클래스에서 가공된 데이터 배열에 푸쉬
   newUserArr.push(newUserInfo.userName, newUserInfo.userCode);
-  newUserInfo.saveUserInfo(newUserArr);
-  // saveUserInfo(newUserArr);
+  saveUserInfo(newUserArr);
 };
 
-// const saveUserInfo = (newUserInfo) => {
-//   // console.log(newUserInfo);
-// };
+const saveUserInfo = (newUserArr) => {
+  const resultUserObject = {
+    userName: newUserArr[0],
+    userCode: newUserArr[1],
+  };
+  // * 저장된 값들 객체화 해서 ../app.mjs - result함수에 다시 뱉어주기
+  app.resultTest(resultUserObject);
+};
